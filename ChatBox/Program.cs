5cc93356aacc47ee2ddf297253f8a4b524545082
Inês Nunes <ines.perdigao.nunes@gmail.com>
2018-03-01 11:00:38 +0000

    Adicionar programa ChatBox

--- ChatBox/Program.cs
+++ ChatBox/Program.cs
@@ -7,6 +7,31 @@ using System.Threading.Tasks;
 namespace ChatBox {
     class Program {
         static void Main(string[] args) {
+            string question;
+
+            do {
+                question = Console.ReadLine();
+
+                switch (question) {
+                    case "Hello":
+                        Console.WriteLine("Heya");
+                        break;
+                    case "How are you?":
+                        Console.WriteLine("I'm gucci, and y- oh wait you can't answer");
+                        break;
+                    case "What's your name?":
+                        Console.WriteLine("That's a very good question, I have no idea");
+                        break;
+                    case "What do you do?":
+                        Console.WriteLine("I don't do anything, I'm useless, but i can tell u that u look stunning");
+                        break;
+                    case "EXIT":
+                        break;
+                    default:
+                        Console.WriteLine("??? que ???");
+                        break;
+                }
+            } while (question != "EXIT");
         }
     }
 }
